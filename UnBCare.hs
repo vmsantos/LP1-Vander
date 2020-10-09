8d895f008f516c13f8047b41068ca9a56d938690
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 16:19:20 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -239,7 +239,7 @@ type PlanoMedicamento = [(Horario,[Medicamento])]
 -}
 -- map (\ x -> if (fst x) == "med1"  then ("med1", quickSort(11:(snd x))) else x) receituarioteste
 
-testeMap :: [(a1, [a2])] -> [(a1, [a2])]
+testeMap :: (Ord a1, Eq a2) => [(a2, [a1])] -> a2 -> a1 -> [(a2, [a1])]
 testeMap r m h = map (\ x -> if (fst x) == m  then (m, quickSort(h:(snd x))) else x) r
 
 removeDuplicates :: Eq a => [a] -> [a]
