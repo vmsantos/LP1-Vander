e2987acb1056619bbf99ebfad9fd40bf509372b1
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 18:18:02 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -270,7 +270,7 @@ teste2 [] = []
 teste2 (h : tail) = quickSort (((teste1 h)) ++ (teste2 tail)) 
 
 percorreLista :: [Horario] -> PlanoMedicamento 
-percorreLista (h:hs) = retornaMedHorario (h [(8,"Quetiapina")]) : percorreLista hs
+percorreLista (h:hs) = (h,retornaMedHorario h (teste2 r)) : percorreLista (h:hs)
 
 geraPlanoReceituario :: [(Medicamento, [Horario])] -> PlanoMedicamento
 geraPlanoReceituario [] = [] 
