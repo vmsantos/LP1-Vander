9f195d5969f817f38bd04b61f31779044e3fe2e9
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 14:51:47 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -245,7 +245,7 @@ removeDuplicates = foldl (\seen x -> if x `elem` seen
                                       else seen ++ [x]) []
 
 teste1 :: (Medicamento,[Horario]) -> [(Horario,[Medicamento])]
-teste1 (m,[h : tail]) = (h,[m]) : tail
+teste1 (m,[h : tail]) = [(h,[m])] ++ tail
 
 geraPlanoReceituario :: Receituario -> PlanoMedicamento
 geraPlanoReceituario [] = []
