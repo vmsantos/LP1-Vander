b2d745e188d5702179db4ca1550e07af6f68b57f
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 18:05:21 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -269,8 +269,8 @@ teste2 :: [(Medicamento, [Horario])] -> [(Horario, Medicamento)]
 teste2 [] = []
 teste2 (h : tail) = quickSort (((teste1 h)) ++ (teste2 tail)) 
 
-geraPlanoReceituario :: Receituario -> PlanoMedicamento
-geraPlanoReceituario [] = []
+geraPlanoReceituario :: [(Medicamento, [Horario])] -> PlanoMedicamento
+geraPlanoReceituario  = []
 
 {- QUESTÃO 8  VALOR: 1,0 ponto
 
