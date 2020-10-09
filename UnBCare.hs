264c6d0e00f8069ab603ea3191a5e0c88085cc66
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 15:28:11 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -253,7 +253,7 @@ teste2 [] = []
 teste2 (x : xs) = quickSort ((teste1 x) ++ (teste2 xs))
 
 teste3 :: Receituario -> PlanoMedicamento -> [(Horario,[Medicamento])]
-teste3 (a,b:bs) (c,d:ds) = [] 
+teste3 ((a,b:bs):tail) ((c,d:ds):tail2) = [] 
 
 geraPlanoReceituario :: Receituario -> PlanoMedicamento
 geraPlanoReceituario [] = []
