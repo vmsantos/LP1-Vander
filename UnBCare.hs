cc416dd5af5b2103a787f88bb83f2c99106c5dbc
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-09 18:59:16 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -286,8 +286,8 @@ retornaMedHorario2 n ((h,m):tail)
    | h == n = m : (retornaMedHorario2 n tail)
    | otherwise = retornaMedHorario2 n tail
 
-listaHorarios :: Receituario -> [Horario]
-listaHorarios r = remDup (quickSort (concat (segundaColuna r)))
+listaMeds :: Receituario -> [Horario]
+listaMeds r = remDup (quickSort (concat (segundaColuna r)))
 
 teste1 :: (Medicamento, [Horario]) -> [(Horario, Medicamento)]
 teste1 (_, []) = []
