c86e7314df399334c6e46bb84421e399ed3d161a
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 16:37:29 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -216,7 +216,7 @@ verificaDupCu ((_,cu):tail)
 verifOrdMedicar :: Plantao -> Bool
 verifOrdMedicar [] = True
 verifOrdMedicar ((_,[(Medicar m)]):tail)   
-  | [m] == quickSort [m] = (verifOrdMedicar tail)
+  | [m] == quickSort [m] = verifOrdMedicar tail
   | otherwise = False
 
 {- 
