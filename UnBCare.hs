5b58913c78ecce44f915b333d86b0024b9eedf7e
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 17:54:33 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -218,7 +218,7 @@ verificaDupCu ((_,cu):tail)
 verifOrdMedicar :: [(Horario,[Cuidado])] -> Bool
 verifOrdMedicar [] = True
 verifOrdMedicar ((_,[(Medicar m)]) : tail)
-  | a == quickSort a = verifOrdMedicar tail
+  | m == quickSort m = verifOrdMedicar tail
   | otherwise = False
   where [(Medicar _)] = [m]
 
