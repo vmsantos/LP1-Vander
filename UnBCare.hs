9b0524457f1f0c89ed78b2ca54fd1bf1766ad5de
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 20:20:28 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -212,7 +212,7 @@ pMedicar :: [Cuidado] -> [Medicamento]
 pMedicar (h:tail)
  -}
 
- intersect :: Eq a => [a] -> [a] -> [a]
+intersect :: Eq a => [a] -> [a] -> [a]
 intersect [] _ = []
 intersect (x:xs) l | elem x l = x : intersect xs l
                    | otherwise = intersect xs l
