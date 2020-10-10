7acd3ce35e4a08071d8765d06919d3b3f21fba6e
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 16:11:21 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -188,7 +188,7 @@ planoValido planomedico
  Um plantão é válido se, e somente se, todas as seguintes condições são satisfeitas:
 
  1. Os horários da lista são distintos e estão em ordem crescente;
- 2. Não há, em um mesmo horário, ocorrência de compra e medicagem de um mesmo medicamento (e.g. `[Comprar m1, Medicar m1 x]`);
+ 2. Não há, em um mesmo horário, ocorrência de compra e medicagem de um mesmo medicamento (e.g. `[Comprar m1 x, Medicar m1]`);
  3. Para cada horário, as ocorrências de Medicar estão ordenadas lexicograficamente.
 
  Defina a função "plantaoValido" que verifica as propriedades acima e cujo tipo é dado abaixo:
