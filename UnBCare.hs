e504dc0de566ffc7919e7762a22fa693ab0fae28
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 14:39:20 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -215,7 +215,8 @@ plantaoValido plantao
   | sequence [verificaOrdHorario2, verificaDupHorario2] plantao == [True, False] = True
   | otherwise = False
 
-pTeste Cuidado = 0
+pTeste :: Cuidado -> p1
+pTeste c = 0
 
 {-
    QUESTÃO 7  VALOR: 1,0 ponto
