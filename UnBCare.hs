603e30c593df395945537eef3edd5a23dcf3f17a
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 18:24:49 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -208,7 +208,7 @@ pMedicar (a:tail)
   | a:tail == [] = []
   | a == (Comprar _ _) = []
   | a == (Medicar m) = m : (pMedicar tail)
-  where a = (Comprar m q)
+  where a = (Comprar _ _)
 
 verificaDupCu :: Plantao -> Bool
 verificaDupCu [(_,[(Comprar _ _)])] = False
