3ce0a9a83ce0c79190e650b1b23e816901ebca64
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 18:11:18 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -204,7 +204,7 @@ pMedicar ((Medicar m):tail) = m : pMedicar tail
 -}
 
 pMedicar :: [Cuidado] -> [Medicamento]
-pMedicar a
+pMedicar _
   | [] = []
   | ((Comprar _ _):_) = []
   | ((Medicar m):tail) = m : pMedicar tail
