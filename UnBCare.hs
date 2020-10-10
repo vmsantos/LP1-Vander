d275b199f2a33b5ba5dc52bd7d3ea897e631b37b
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 18:11:03 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -205,9 +205,9 @@ pMedicar ((Medicar m):tail) = m : pMedicar tail
 
 pMedicar :: [Cuidado] -> [Medicamento]
 pMedicar
-  | pMedicar [] = []
-  | pMedicar ((Comprar _ _):_) = []
-  | pMedicar ((Medicar m):tail) = m : pMedicar tail
+  | [] = []
+  | ((Comprar _ _):_) = []
+  | ((Medicar m):tail) = m : pMedicar tail
 
 verificaDupCu :: Plantao -> Bool
 verificaDupCu [(_,[(Comprar _ _)])] = False
