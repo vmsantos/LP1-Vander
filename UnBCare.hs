1422ae2a46a8093c77d3a7868191739157fc80ee
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 23:30:17 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -202,9 +202,9 @@ asdteste ((_,c:cs):tail)
 
  -}
 
-lTeste (a) = zipWith (==) (filter (\x -> case x of 
+lTeste a = (filter (\x -> case x of 
                               (Comprar _ _) -> True 
-                              (Medicar _) -> False) a) a
+                              (Medicar _) -> False) a)
 
 pMedicar :: [Cuidado] -> [Medicamento]
 pMedicar [] = []
