ed3df3bfcbf90bffa485f13cdb56553b1241ad47
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-10 22:35:37 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -202,7 +202,7 @@ asdteste ((_,c:cs):tail)
 
  -}
 
-lTeste (a) = zipWith (\True -> y) (filter (\x -> case x of 
+lTeste (a) = zipWith (==) (filter (\x -> case x of 
                               (Comprar _ _) -> True 
                               (Medicar _) -> False) a) a
 
