9a5531cbfe90b6babedd58ee4f386bb44d5fca84
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-10-12 02:55:22 -0300

    Gitacora saved 1 changed files

--- UnBCare.hs
+++ UnBCare.hs
@@ -413,7 +413,7 @@ geraPlantao  [] _ = []
 geraPlantao ((h,m):tail) _ = (h,geraCuidados m) : (geraPlantao tail [])
 
 geraPlantaoAux :: PlanoMedicamento -> (Horario, [Cuidado])
-geraPlantaoAux ((h,_):tail) = (h-1, (geraCompras ((h,_):tail)))
+geraPlantaoAux ((h,_):tail) = (h-1, geraCompras ((h,_):tail))
 
 plantaoCorreto :: PlanoMedicamento -> EstoqueMedicamentos -> Plantao
 plantaoCorreto  [] _ = []
