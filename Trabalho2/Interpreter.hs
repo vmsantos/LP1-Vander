4e608f70ee1c40d5ecf5bb891faedf3d33ffcfe0
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-11-21 20:24:30 -0300

    Gitacora saved 1 changed files

--- Trabalho2/Interpreter.hs
+++ Trabalho2/Interpreter.hs
@@ -15,7 +15,7 @@ exemploReturn1 = Prog [Fun Tint (Ident "main") []
                     [SDec (Dec Tint (Ident "c")), 
                     SDec (Dec Tint (Ident "soma")),
                     SAss (Ident "c") (EInt 10),
-                    SAss (Ident "somaa") (EInt 0),
+                    SAss (Ident "soma") (EInt 0),
                     SRepeat (SBlock [SAss (Ident "soma") (EAdd (EVar (Ident "soma")) (EVar (Ident "c"))),
                                      SAss (Ident "c") (ESub (EVar (Ident "c")) (EInt 1))])
                         (EVar (Ident "c")),
