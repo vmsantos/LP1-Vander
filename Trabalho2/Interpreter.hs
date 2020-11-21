f79b9fe7b7c09551403c53f3c07c1fe09259f4d6
Vinicius Melo <viniciusmelo@bsd.com.br>
2020-11-21 20:16:01 -0300

    Gitacora saved 1 changed files

--- Trabalho2/Interpreter.hs
+++ Trabalho2/Interpreter.hs
@@ -118,6 +118,9 @@ execute environment x = case x of
    SIf exp stmT stmE -> if ( i (eval environment exp) /= 0) 
                           then execute environment stmT
                           else execute environment stmE
+   SRepeat stm exp -> if ( i (eval environment exp) /= 0) 
+                      then execute (execute environment stm) (SRepeat stm exp)
+                      else environment                       
 
 
 eval :: Environment -> Exp -> Valor
